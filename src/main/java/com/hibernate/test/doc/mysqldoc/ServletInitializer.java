e75a50749e4e09d5109e39fdb4f3de0ad3b10190
3amer <mahmedabdallah@fci.helwan.edu.eg>
2019-03-24 13:41:07 +0200

    Post Mapping JSon

--- src/main/java/com/hibernate/test/doc/mysqldoc/ServletInitializer.java
+++ src/main/java/com/hibernate/test/doc/mysqldoc/ServletInitializer.java
@@ -4,15 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping(value = "/")
 public class ServletInitializer extends SpringBootServletInitializer {
 
-        @Autowired
-        private CustomerInterface customerCrud ;
-    
+    @Autowired
+    private CustomerInterface customerCrud;
+
     @GetMapping(value = "customer")
     public Customer getUser() {
         Customer c = new Customer();
@@ -20,11 +23,26 @@ public class ServletInitializer extends SpringBootServletInitializer {
         c.setFirstName("3amer");
         c.setLastName("3amer");
         customerCrud.save(c);
+        System.out.println(customerCrud.count());
         return c;
     }
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(MysqldocApplication.class);
-	}
+
+    @PostMapping(value = "insert", consumes = "application/json")
+
+    public void saveCustomer(@RequestBody Customer customer) {
+        try {
+            System.out.println("Hello from Servlet Iam inside Try .........");
+            customerCrud.save(customer);
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MysqldocApplication.class);
+    }
 
 }
